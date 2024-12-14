e1e3990e35555254d9ac414fa7629e389df60f25
sheriffkule <zeljko225@yahoo.com>
2024-12-14 17:46:43 +0100

    added routes and search functionality

--- api.js
+++ api.js
@@ -3,9 +3,17 @@
 const api_key = '4ca3da810ec7ce129a86d9a6a7b0d706';
 
 export const fetchData = function (URL, callback) {
-	fetch(`${URL}$appid=${api_key}`)
-		.then((res) => res.json())
-		.then((data) => callback(data));
+	fetch(`${URL}&appid=${api_key}`)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error(`HTTP error! status: ${res.status}`);
+			}
+			return res.json();
+		})
+		.then((data) => callback(data))
+		.catch((error) => {
+			alert('Fetch error:', error);
+		});
 };
 
 export const url = {
@@ -19,9 +27,9 @@ export const url = {
 		return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
 	},
 	reverseGeo(lat, lon) {
-		return `https://api.opencagedata.com/reverse/v1/json?q=${lat}+${lon}&limit=5`;
+		return `https://api.opencagedata.com/reverse/v1/json?q=${lat}+${lon}&limit=15`;
 	},
 	geo(query) {
-		return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+		return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=15`;
 	},
 };
--- app.js
+++ app.js
@@ -1,3 +1,109 @@
+'use strict';
+
+import { fetchData, url } from './api.js';
+import * as module from './module.js';
+
+const addEventOnElements = function (elements, eventType, callback) {
+	for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+const searchView = document.querySelector('[data-search-view]');
+const searchTogglers = document.querySelectorAll('[data-search-toggler]');
+
+if (searchView && searchTogglers.length > 0) {
+	const toggleSearch = () => searchView.classList.toggle('active');
+	addEventOnElements(searchTogglers, 'click', toggleSearch);
+}
+
+const searchField = document.querySelector('[data-search-field]');
+const searchResult = document.querySelector('[data-search-result]');
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener('input', function () {
+	searchTimeout ?? clearTimeout(searchTimeout);
+
+	if (!searchField.value) {
+		searchResult.classList.remove('active');
+		searchResult.innerHTML = '';
+		searchField.classList.remove('searching');
+	} else {
+		searchField.classList.add('searching');
+	}
+
+	if (searchField.value) {
+		searchTimeout = setTimeout(() => {
+			fetchData(url.geo(searchField.value), function (locations) {
+				searchField.classList.remove('searching');
+				searchResult.classList.add('active');
+				searchResult.innerHTML = `
+                <ul class="view-list" data-search-list></ul>
+                `;
+
+				const items = [];
+
+				for (const { name, lat, lon, country, state } of locations) {
+					const searchItem = document.createElement('li');
+					searchItem.classList.add('view-item');
+
+					searchItem.innerHTML = `
+                    <span class="m-icon">location_on</span>
+
+                    <div>
+                        <p class="item-title">${name}</p>
+                        <p class="label-2 item-subtitle">${state || ''}, ${country}</p>
+                    </div>
+
+                    <a href="#weather?lat=${lat}&${lon}" class="item-link has-state" aria-label='${name} weather' data-search-toggler></a>
+                    `;
+
+                    searchResult.querySelector('[data-search-list]').appendChild(searchItem);
+                    items.push(searchItem.querySelector('[data-search-toggler]'))
+				}
+			});
+		}, searchTimeoutDuration);
+	}
+});
+
+const container = document.querySelector('[data-container]');
+const loading = document.querySelector('[data-loading]');
+const currentLocationBtn = document.querySelector('[data-current-location-btn]');
+const errorContent = document.querySelector('[data-error-content]');
+
+export const updateWeather = function (lat, lon) {
+    
+    loading.style.display = 'grid';
+    container.style.overflowY = 'hidden';
+    container.classList.contains('fade-in') ?? container.classList.remove('fade-in');
+    errorContent.style.display = 'none';
+
+    const currentWeatherSection = document.querySelector('[data-current-weather]');
+    const highlightSection = document.querySelector('[data-highlights]');
+    const hourlySection = document.querySelector('[data-hourly-forecast]');
+    const forecastSection = document.querySelector('[data-5-day-forecast]');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const year = document.getElementById('year');
 const thisYear = new Date().getFullYear();
 
